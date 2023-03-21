295051befc926e978203003c74ca7afeac9e0414
xingqiangqiang <qiangqiang.xing@realai.ai>
2023-03-21 14:01:53 +0800

    feat(view): initial

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,14 @@
 import reducer from '@/config/reducer';
 import { configureStore } from '@reduxjs/toolkit';
+import { createLogger } from 'redux-logger';
+
+const logger = createLogger({ collapsed: true });
 
 const store = configureStore({
     reducer: {
         ...reducer,
     },
+    middleware: process.env.NODE_ENV !== 'production' ? [logger] : [],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
