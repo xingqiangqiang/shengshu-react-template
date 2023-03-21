9c659c9181f82d12565a3149bc2794eef47fb672
xingqiangqiang <qiangqiang.xing@realai.ai>
2023-03-21 14:44:42 +0800

    feat(view): initial

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,8 +1,10 @@
+import LottiePlayerAnimation from '@/components/lottie-player-animation';
 import ReduxTest from '@/pages/redux-test/main';
 import { Button } from 'antd';
 import dayjs from 'dayjs';
 import React from 'react';
 
+import animation from './animation.json';
 import styles from './index.module.scss';
 
 const AppMenu: React.FC = () => {
@@ -12,6 +14,7 @@ const AppMenu: React.FC = () => {
             <Button type="primary">确定</Button>
             {dayjs('2022-12-12 13:00:00').format('YYYY-MM-DD A HH:mm:ss')}
             <ReduxTest />
+            <LottiePlayerAnimation jsonFile={animation} playState loop />
         </div>
     );
 };
