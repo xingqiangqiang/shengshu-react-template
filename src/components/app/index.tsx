c6e4a4e4f9b048d2c90d856daa1e70ae5312ed15
xingqiangqiang <qiangqiang.xing@realai.ai>
2023-03-21 14:10:45 +0800

    feat(view): initial

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -8,8 +8,7 @@ import styles from './index.module.scss';
 const AppMenu: React.FC = () => {
     return (
         <div className={styles.app}>
-            <div className="box1">box1</div>
-            <div className="box2">box2</div>
+            <div className="box">box</div>
             <Button type="primary">确定</Button>
             {dayjs('2022-12-12 13:00:00').format('YYYY-MM-DD A HH:mm:ss')}
             <ReduxTest />
