66103fe99724394bf75b9f7cf5d34dfca6860066
qiangqiang.xing <qiangqiang.xing@realai.ai>
2023-04-13 20:05:38 +0800

    feat(view): 添加环境变量支持

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-/** @typing {import('vite').UserConfig} */
+/** @typing {import("vite").UserConfig} */
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
@@ -6,82 +6,83 @@ import { createHtmlPlugin } from 'vite-plugin-html';
 import progress from 'vite-plugin-progress';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
-const projectName = '测试title';
+const projectName = '生数科技web端title';
 const port = 8080;
 const base = '/';
 console.log('当前环境:', process.env.NODE_ENV);
 
 export default defineConfig({
-    base,
-    build: {
-        rollupOptions: {
-            output: {
-                assetFileNames: '[ext]/[name].[hash].[ext]',
-            },
-        },
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                drop_console: true,
-                drop_debugger: true,
-            },
-        },
+  base,
+  envDir: 'src',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: '[ext]/[name].[hash].[ext]',
+      },
     },
-    server: {
-        port,
-        open: `http://localhost:${port}${base}`,
-        proxy: {
-            '/mock/api/v1': {
-                target: 'http://yapi.realai-inc.cn',
-                // 有base时要重写代理
-                // rewrite: (path) => path.replace(/^\/base/, ''),
-                // changeOrigin: true,
-            },
-        },
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
     },
-    plugins: [
-        react(),
-        createSvgIconsPlugin({
-            // 指定需要缓存的图标文件夹
-            iconDirs: [resolve(process.cwd(), 'src/images/svg')],
-            // 指定symbolId格式
-            symbolId: 'icon-[dir]-[name]',
-            /**
-             * custom dom id
-             * @default: __svg__icons__dom__
-             */
-            customDomId: '__svg__icons__dom__',
-        }),
-        // 详见 https://github.com/vbenjs/vite-plugin-html
-        createHtmlPlugin({
-            minify: true,
-            entry: '/src/main.tsx',
-            template: 'index.html',
-            inject: {
-                data: {
-                    title: projectName,
-                    injectScript: `<script src='./inject.js' type='module'></script>`,
-                },
-            },
-        }),
-        progress(),
-    ],
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src'),
-        },
+  },
+  server: {
+    port,
+    open: `http://localhost:${port}${base}`,
+    proxy: {
+      '/mock/api/v1': {
+        target: 'http://yapi.realai-inc.cn',
+        // 有base时要重写代理
+        // rewrite: (path) => path.replace(/^\/base/, ''),
+        // changeOrigin: true,
+      },
     },
-    css: {
-        modules: {
-            generateScopedName: '[path]_[name]_[local]_[hash:base64:5]',
-            hashPrefix: 'prefix',
+  },
+  plugins: [
+    react(),
+    createSvgIconsPlugin({
+      // 指定需要缓存的图标文件夹
+      iconDirs: [resolve(process.cwd(), 'src/images/svg')],
+      // 指定symbolId格式
+      symbolId: 'icon-[dir]-[name]',
+      /**
+       * custom dom id
+       * @default: __svg__icons__dom__
+       */
+      customDomId: '__svg__icons__dom__',
+    }),
+    // 详见 https://github.com/vbenjs/vite-plugin-html
+    createHtmlPlugin({
+      minify: true,
+      entry: '/src/main.tsx',
+      template: 'index.html',
+      inject: {
+        data: {
+          title: projectName,
+          injectScript: `<script src="./inject.js" type="module"></script>`,
         },
-        preprocessorOptions: {
-            scss: {
-                additionalData: `@import "@/assats/style/public.scss";
+      },
+    }),
+    progress(),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  css: {
+    modules: {
+      generateScopedName: '[path]_[name]_[local]_[hash:base64:5]',
+      hashPrefix: 'prefix',
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assats/style/public.scss";
           @import "@/assats/style/variables.scss";
           @import "@/assats/style/mixin.scss";`,
-            },
-        },
+      },
     },
+  },
 });
